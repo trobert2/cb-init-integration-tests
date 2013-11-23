79ea86280f45c90d3da77fb5695c9227388f01e5
trobert2 <rtingirica@cloudbasesolutions.com>
2013-11-23 12:55:28 +0200

    Withoud administrator password

--- utils/prepare.sh
+++ utils/prepare.sh
@@ -20,12 +20,12 @@ echo "Image size "$imageSize
 
 network_ids=()
 function get_network_id  {
-network_id=`quantum subnet-show $1 | awk '{if ( NR == 13 ) print $4}'`
+network_id=`neutron subnet-show $1 | awk '{if ( NR == 13 ) print $4}'`
 network_ids+=($network_id)
 }
 
 function check_public_network {
-public=`quantum net-show $1 | awk '{if (NR ==10) print $4}'`
+public=`neutron net-show $1 | awk '{if (NR ==10) print $4}'`
 if [ "$public" ==  "True" ] ; then
  return 0
 else
@@ -33,7 +33,7 @@ else
 fi
 }
 
-subnet_ids=($(quantum subnet-list | awk '{print $2}'))
+subnet_ids=($(neutron subnet-list | awk '{print $2}'))
 
  for i in "${subnet_ids[@]:1}"
  do
@@ -60,10 +60,10 @@ else
 	echo "Using existing keypair "$keypair
 fi
 
-floatingip_id=`quantum floatingip-create $public_network | awk '{if (NR==8) print $4}'`
+floatingip_id=`neutron floatingip-create $public_network | awk '{if (NR==8) print $4}'`
 echo $floatingip_id
 
-ip=`quantum floatingip-show $floatingip_id | awk '{if (NR==5) print $4}'`
+ip=`neutron floatingip-show $floatingip_id | awk '{if (NR==5) print $4}'`
 
 #echo  "[BaseVmConf]
 #flavor_id=$flavor_id
@@ -91,13 +91,13 @@ if [ $? -ne 0 ];then sed -i "s/imageSize=.*/imageSize=$imageSize/g" $2;fi
 vm_id=`nova boot --flavor $flavor_id --image $image_id --key-name $keypair --nic net-id=$private_network --user-data $3 $vm_name --poll | awk '{if (NR==15) print $4}'`
 echo "Vm created with id "$vm_id
 
-vmdevice_id=`quantum port-list --fields id --device_id $vm_id | awk '{if (NR == 4) print $2}'`
+vmdevice_id=`neutron port-list --fields id --device_id $vm_id | awk '{if (NR == 4) print $2}'`
 echo "Vm device id is "$vmdevice_id
 
-quantum floatingip-associate $floatingip_id $vmdevice_id
+neutron floatingip-associate $floatingip_id $vmdevice_id
 tenant_id=`nova show $vm_id | awk '{if (NR==20) print $4}'`
 ip_tenant_id=""
 while [ "$ip_tenant_id" != "$tenant_id" ]; do
        echo "waiting for floating ip to attach.."
-       ip_tenant_id=`quantum floatingip-show $floatingip_id | awk '{if (NR==10) print $4}'`
+       ip_tenant_id=`neutron floatingip-show $floatingip_id | awk '{if (NR==10) print $4}'`
 done
